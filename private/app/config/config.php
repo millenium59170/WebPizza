3d001d744bee9fa21df24c2861efcce648f0dcd6
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-19 15:11:10 +0100

    -

--- private/app/config/config.php
+++ private/app/config/config.php
@@ -3,7 +3,7 @@
  * Fichier de config general de l'app
  * 
  * 1.def des constantes
- * 2.
+ * 2.def des variable d'environnement d'execution
  * 3.
  */
 
@@ -13,3 +13,19 @@
 
   //WEBSITE_TITLE : Definition du titre di site
   define('WEBSITE_TITLE', "WebPizza !");
+
+/**
+ * 2.def des variable d'environnement d'execution
+ */
+
+//environement de dev ou prod
+//les valeur peuvent etre : prod ou dev
+// par defaut on considere que l'app s'execute en environnement de PROD
+$env = "prod";
+
+//listes des domaines que lon consideres comme etant des environnement de developpement
+$dev_domains = [
+"127.0.0.1",
+"localhost",
+"webpizza.local"
+];
