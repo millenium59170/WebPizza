bc90b4f63fc6d69230727cdbaa7f4f8f0e8f4000
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-19 14:56:39 +0100

    3em up

--- private/app/config/config.php
+++ private/app/config/config.php
@@ -1,1 +1,15 @@
-1
+<?php
+/**
+ * Fichier de config general de l'app
+ * 
+ * 1.def des constantes
+ * 2.
+ * 3.
+ */
+
+ /**
+  * 1 definition des constantes
+  */
+
+  //WEBSITE_TITLE : Definition du titre di site
+  define('WEBSITE_TITLE', "WebPizza !");
