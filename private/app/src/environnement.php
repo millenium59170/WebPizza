2ad1c8b87f4554aed52ef9ced1eaa563d57abe95
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-19 16:41:03 +0100

    -

--- private/app/src/environnement.php
+++ private/app/src/environnement.php
@@ -1,1 +1,17 @@
-2
+<?php
+/**
+ * fichier de definition de l'environnement d'execution de l'app
+ */
+
+ //dans le cas ou la variable $dev_domains n'est pas defini (dans le fichier config.php)
+ //on initialise la variable $dev_domains avec un tableau vide 
+
+ if (!isset($dev_domains)) {
+     $dev_domains = [];
+ }
+
+
+ 
+
+ echo "--------<br>";
+ echo "valeur de \$env = ".$env;
