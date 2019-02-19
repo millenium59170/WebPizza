fa06c454a6fa9957821362e678010882e0e2489d
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-19 14:28:10 +0100

    ajout des fichier

---
+++ private/app/src/app.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Fichier d'execution de l'app
+ */
+/**
+ * intergration du config
+ */
+require_once "../private/app/config/config.php";
+
+require_once "../private/app/src/environnement.php";
+
+require_once "../private/app/src/err_reporting.php";
+
+require_once "../private/app/src/db_connect.php";
+
+require_once "../private/app/src/routing.php";
+
+require_once "../private/app/src/compile.php";
+
