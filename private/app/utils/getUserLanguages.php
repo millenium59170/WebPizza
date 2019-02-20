b7055a243f54ce37e16351a02b7241ae9fc7af12
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-20 15:23:14 +0100

    -

--- private/app/utils/getUserLanguages.php
+++ private/app/utils/getUserLanguages.php
@@ -1,18 +1,24 @@
 <?php
-/**
- * Détermine les langues de l'utilisateur
- * 
- */
-if (!function_exists('getUserLanguage')) 
-{
-    function getUserLanguages(){
-        $languages_str = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-        $languages_arr = explode(",", $languages_str);
 
-       foreach($languages_arr as $key => $lang){
-           echo $lang."<br>";
+/**
+* Determine les langues de l'utilisateur
+*
+*/
+
+   if(!function_exists('getUserLanguages')){
+       /**
+        * Retourne les langues de l'utilisateur.
+        *
+        * @return mixed
+        */
+       function getUserLanguages($all = false){
+           $languages = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+           foreach($languages as $key => $lang){
+               $languages[$key] = explode(";",$lang)[0];
+           }
+           return ($all ? $languages : $languages[0]);
        }
-       dump($languages_arr);
-    }
-}
-getUserLanguages();
+   }
+
+   dump(getUserLanguages());
+?>
