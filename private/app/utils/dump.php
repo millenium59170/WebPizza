dd82e187d2ff09e2f280d46ab9f995c2cab0d0ab
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-20 12:29:23 +0100

    -

---
+++ private/app/utils/dump.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Dump permet d'afficher une donner entre les balise
+ * 
+ */
+
+ 
+
+/**
+ * Undocumented function
+ *
+ * @param [type] $data
+ * @return void
+ */
+function dump($data){
+    echo"<pre>";
+print_r($data);
+echo"<pre>";
+}
