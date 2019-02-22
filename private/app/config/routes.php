e98ec71e4035f0c707288c3dda9fab24ba1ef76a
Dekeuwer Laurent <millenium59170@gmail.com>
2019-02-22 09:40:23 +0100

    -

--- private/app/config/routes.php
+++ private/app/config/routes.php
@@ -15,8 +15,26 @@ $routes = [
     // Route Index (page d'accueil du site)
     ["homepage", "/", "homepage:index", ["HEAD","GET"]],
 
+    // Liste des produits
+    ["pizzas", "/pizzas", "products:pizzas", ["HEAD","GET"]],
+    ["salads", "/salades", "products:salads", ["HEAD","GET"]],
+    ["desserts", "/desserts", "products:desserts", ["HEAD","GET"]],
+    ["drinks", "/boissons", "products:drinks", ["HEAD","GET"]],
+    ["menus", "/menus", "products:menus", ["HEAD","GET"]],
+
     // Page de contact
-    ["contact", "/contact", "contact:index", ["HEAD","GET"]],
+    ["contact", "/contact", "contact:index", ["HEAD","GET","POST"]],
+
+    // Pages de sécurité
+    ["login", "/login", "security:login", ["HEAD","GET","POST"]],
+    ["register", "/register", "security:register", ["HEAD","GET","POST"]],
+    ["forgotten_password", "/forgotten-password", "security:forgotten_password", ["HEAD","GET","POST"]],
+
+    // Page de commande
+    ["order", "/order", "order:index", ["HEAD","GET"]],
+
+    // Profil utilisateur
+    ["account", "/mon-compte", "account", ["HEAD","GET"]],
 
     // ---
 
---
+++ private/src/controllers/homepage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Fichier qui gère la page d'accueil
+ */
+
+/**
+ * index
+ */
+function homepage_index() 
+{
+    // Intégration de la vue
+    include_once "../private/src/views/homepage/index.php";
+}
